f48b16242ffeb4cfd81f355b71932e24f16ac194
seoywa <132980713+seoywa@users.noreply.github.com>
2025-04-23 14:27:03 +0700

    Created dashboard, implementing chart of used space

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,12 +1,13 @@
 "use server";
 
-import { createAdminClient } from "@/appwrite";
+import { createAdminClient, createSessionClient } from "@/appwrite";
 import { appwriteConfig } from "@/appwrite/config";
 import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
 import { InputFile } from "node-appwrite/file";
 import { getCurrentUser } from "./user.actions";
+import { Handlee } from "next/font/google";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -189,3 +190,45 @@ export const deleteFile = async ({
     handleError(error, "Failed to share file.");
   }
 };
+
+export const getTotalSpaceUsed = async () => {
+  try {
+    const { databases } = await createSessionClient();
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error('User is not authenticated');
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      [Query.equal('owner', [currentUser.$id])],
+    );
+
+    const totalSpace = {
+      image: {size: 0, latestDate: ''},
+      document: {size: 0, latestDate: ''},
+      video: {size: 0, latestDate: ''},
+      audio: {size: 0, latestDate: ''},
+      other: {size: 0, latestDate: ''},
+      used: 0,
+      all: 2 * 1024 * 1024 * 1024 
+      //2GB of available bucket storage
+    };
+
+    files.documents.forEach((file) => {
+      const fileType = file.type as FileType;
+
+      totalSpace[fileType].size += file.size;
+      totalSpace.used += file.size;
+
+      if (!totalSpace[fileType].latestDate || new Date(file.$updatedAt) > new Date(totalSpace[fileType].latestDate)) {
+        totalSpace[fileType].latestDate = file.$updatedAt;
+      }
+    });
+
+    return parseStringify(totalSpace);
+
+  } catch (error) {
+    handleError(error, 'Failed to calculate total space used.')
+  }
+}
